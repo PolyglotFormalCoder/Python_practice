27e43fd7af25c6cc1117734d6373419fee25fad0
PolyglotFormalCoder <>
2020-09-30 17:18:48 +0200

    Update ex14.py

--- ex14.py
+++ ex14.py
@@ -1,7 +1,7 @@
 from sys import argv
-
-script, user_name = argv
-prompt = '> '
+from cryptography.fernet import Fernet
+script, user_name, password = argv
+prompt = '>> '
 
 print(f"Hi {user_name}, I'm the {script} script.")
 print("I'd like to ask you a few questions.")
@@ -19,3 +19,11 @@ Alright, so you said {likes} about liking me.
 You live in {lives}. Not sure where that is.
 And you have a {computer} computer. Nice.
 """)
+
+key = Fernet.generate_key()
+f = Fernet(key)
+token = f.encrypt(bytes(password, 'utf-8'))
+print(bytes(password, 'utf-8'))
+print(token)
+print(f.decrypt(token))
+
